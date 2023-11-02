ab47dcbcfa20ef478f94289555ca1d633d59513c
joseph.kim <joseph0926@naver.com>
2023-11-02 14:56:09 +0900

    #4 shadcn-ui 추가

--- store/app/page.tsx
+++ store/app/page.tsx
@@ -1,21 +1,20 @@
-import Image from "next/image";
-import Login from "./login/index";
-import Update from "./update";
-import Notice from "./notice";
-import Logo from "./logo";
+import Login from './login/index';
+import Update from './update';
+import Notice from './notice';
+import Logo from './logo';
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-around w-full h-full min-w-[1300px] min-h-[800px] overflow-auto">
+    <main className="flex h-full min-h-[800px] w-full min-w-[1300px] flex-col justify-around overflow-auto">
       <section className="flex justify-around">
-        <section className="flex justify-center items-center">
+        <section className="flex items-center justify-center">
           <Logo />
         </section>
-        <section className="flex justify-center items-center ">
-          <img src="" alt="image" className="w-[600px] h-[400px] bg-red-300" />
+        <section className="flex items-center justify-center ">
+          <img src="" alt="image" className="h-[400px] w-[600px] bg-red-300" />
         </section>
       </section>
-      <section className="flex justify-around items-center">
+      <section className="flex items-center justify-around">
         <Login />
         <Update />
         <Notice />
