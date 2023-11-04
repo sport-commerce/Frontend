9294c361a483567fe5641ac46795558e9b95bed0
joseph0926 <joseph0926@naver.com>
2023-11-04 14:40:12 +0900

    #6 폴더 구조 변경

---
+++ store/app/(root)/(routes)/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main className="flex h-full min-h-[800px] w-full min-w-[1300px] flex-col justify-around overflow-auto">
+      <section className="flex justify-around">
+        <section className="flex items-center justify-center">
+          <Logo />
+        </section>
+        <section className="flex items-center justify-center ">
+          <img src="" alt="image" className="h-[400px] w-[600px] bg-red-300" />
+        </section>
+      </section>
+      <section className="flex items-center justify-around">
+        <Login />
+        <Update />
+        <Notice />
+      </section>
+    </main>
+  );
+}
