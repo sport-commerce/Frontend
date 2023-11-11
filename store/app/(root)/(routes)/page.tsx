030c6ba00b5fee2e5508ae494316c03434a20259
김영훈 <joseph0926@naver.com>
2023-11-11 14:29:35 +0900

    #8 폴더/파일 구조 변경

--- store/app/(root)/(routes)/page.tsx
+++ store/app/(root)/(routes)/page.tsx
@@ -1,13 +1,11 @@
 import Intro from '@/app/(root)/_components/Intro';
 import Hero from '@/app/(root)/_components/Hero';
-import MainNavbar from '../_components/MainNavbar';
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <MainNavbar />
+    <>
       <Hero />
       <Intro />
-    </div>
+    </>
   );
 }
